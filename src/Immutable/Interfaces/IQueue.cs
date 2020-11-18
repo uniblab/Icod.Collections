9bd3cfdf47b510e4465926d584718c26b49e481c
uniblab <uniblab@hotmail.com>
2020-11-17 19:49:34 -0500

    copyright/license updates
    Signed-off-by: uniblab <uniblab@hotmail.com>

--- src/Immutable/Interfaces/IQueue.cs
+++ src/Immutable/Interfaces/IQueue.cs
@@ -1,5 +1,6 @@
 ﻿// Copyright 2008, Eric Lippert
 // Copyright 2020, Timothy J. Bruce
+// This file is licensed under the terms of the GNU LESSER GENERAL PUBLIC LICENSE, version 2.1, February 1999
 
 namespace Icod.Collections.Immutable {
 
--- src/Immutable/Stack`1.cs
+++ src/Immutable/Stack`1.cs
@@ -1,4 +1,5 @@
 ﻿// Copyright 2020, Timothy J. Bruce
+// This file is licensed under the terms of the GNU LESSER GENERAL PUBLIC LICENSE, version 2.1, February 1999
 
 namespace Icod.Collections.Immutable {
 
