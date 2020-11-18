fd2a8876fb8646c7eca376aa04fc58a1e99f7cfa
uniblab <uniblab@hotmail.com>
2020-11-17 19:49:02 -0500

    initial add
    Signed-off-by: uniblab <uniblab@hotmail.com>

---
+++ src/Immutable/KeyComparer`1.cs
@@ -0,0 +1,21 @@
+﻿// Copyright 2020 Timothy J. Bruce
+// This file is licensed under the terms of the GNU LESSER GENERAL PUBLIC LICENSE, version 2.1, February 1999
+
+using System.Linq;
+
+namespace Icod.Collections.Immutable {
+
+	public static class KeyComparer {
+
+		public static System.Int32 CompareKeys<K>( this K key, K other ) where K : System.IComparable<K> {
+			return ( System.Object.ReferenceEquals( null, key ) )
+				? ( System.Object.ReferenceEquals( null, other ) )
+					? 0
+					: -other.CompareTo( key )
+				: key.CompareTo( other )
+			;
+		}
+
+	}
+
+}
